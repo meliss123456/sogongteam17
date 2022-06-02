8095a2ddb594584ce2501ed04b517e0edb3ffc3a
S-J-Kim <hedro0458@gmail.com>
2022-06-02 23:39:31 +0900

    file stream example

--- implementation/signup.cpp
+++ implementation/signup.cpp
@@ -5,8 +5,12 @@
 
 using namespace std;
 
-Signup::Signup(UserRepository* userlist) {
+Signup::Signup(UserRepository *userlist, ifstream *fin, ofstream *fout)
+{
+	// collection class 및 file stream 객체 초기화
 	this->userlist = userlist;
+	this->fin = fin;
+	this->fout = fout;
 
 	SignupUI *UI = new SignupUI(this);
 	this->suUI = UI;
@@ -14,23 +18,24 @@ Signup::Signup(UserRepository* userlist) {
 	UI->completeSignup();
 }
 
-User* Signup::createNewUser() {
+User *Signup::createNewUser()
+{
 	User *newUser = new User(this->suUI->ID, this->suUI->PW, this->suUI->name, this->suUI->SID);
 	userlist->addUser(newUser);
 
 	return newUser;
 }
 
-
-SignupUI::SignupUI(Signup* control){
+SignupUI::SignupUI(Signup *control)
+{
 	this->SID = 0;
 	this->signupControl = control;
 }
 
-void SignupUI::completeSignup(){
-	cin >> this->name >> this->SID >> this->ID >> this->PW;
+void SignupUI::completeSignup()
+{
+	this->signupControl->getFin() >> this->name >> this->SID >> this->ID >> this->PW;
 
 	this->signupControl->createNewUser();
-	cout << "> " << this->name << this->SID << this->ID << this->PW << endl;
-
+	this->signupControl->getFout() << "> " << this->name << this->SID << this->ID << this->PW << endl;
 }
--- implementation/signup.h
+++ implementation/signup.h
@@ -6,25 +6,35 @@ using namespace std;
 class Signup;
 class UserRepository;
 
-class SignupUI{
+class SignupUI
+{
 public:
-	Signup* signupControl;
+	Signup *signupControl;
 	string ID;
 	string PW;
 	string name;
 	int SID;
 
-	SignupUI(Signup*);
+	SignupUI(Signup *);
 	void completeSignup();
 };
 
-class Signup{
+class Signup
+{
 private:
-	SignupUI* suUI;
+	SignupUI *suUI;
 	UserRepository *userlist;
 
-public:
-	Signup(UserRepository*);
-	User* createNewUser();
-};
+	// 파일 입출력 객체 포인터 선언
+	ifstream *fin;
+	ofstream *fout;
 
+public:
+	// main 함수의 진입점 함수에서 파일 스트림 객체를 매개변수로 받아서 멤버로 초기화
+	Signup(UserRepository *, ifstream *, ofstream *);
+	User *createNewUser();
+
+	// 파일스트림 객체의 getter
+	ifstream *getFin() { return this->fin; }
+	ofstream *getFout() { return this->fout; }
+};
